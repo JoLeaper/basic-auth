0de04aca417bac89230609912eaec8f53f47f12e
JoLeaper <joleap123@gmail.com>
2020-06-15 14:59:54 -0700

    get all test written for auctions

--- __tests__/auctions.test.js
+++ __tests__/auctions.test.js
@@ -7,6 +7,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 const User = require('../lib/models/User');
+const Auction = require('../lib/models/Auction');
 
 describe('basic-auth routes', () => {
   beforeAll(async() => {
@@ -23,6 +24,82 @@ describe('basic-auth routes', () => {
     return mongod.stop();
   });
 
+  it('creates an auction', async() => {
+    const user1 = await User.create({
+      email: 'jj@gmail.com',
+      password: 'jjissupercool'
+    });
+
+    return request(app)
+      .post('/api/v1/auctions/')
+      .send({
+        title: 'The First Auction!',
+        description: 'This is the first auction!',
+        quantity: 25,
+        endDate: Date.now(),
+        user: user1._id
+      }).then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          title: expect.any(String),
+          description: expect.any(String),
+          quantity: expect.any(Number),
+          endDate: expect.any(String),
+          bids: [],
+          user: user1._id.toString(),
+          __v: 0
+        });
+      });
+  });
+
+  it('gets all auctions', async() => {
+    const user1 = await User.create({
+      email: 'jj@gmail.com',
+      password: 'jjissupercool'
+    });
+
+    await Auction.create({
+      title: 'The First Auction!',
+      description: 'This is the first auction!',
+      quantity: 25,
+      endDate: Date.now(),
+      user: user1._id
+    },
+    {
+      title: 'The SECOND Auction!',
+      description: 'This is the SECOND auction!',
+      quantity: 25,
+      endDate: Date.now(),
+      user: user1._id
+    });
+
+    return request(app)
+      .get('/api/v1/auctions/')
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: expect.anything(),
+          title: expect.any(String),
+          description: expect.any(String),
+          quantity: expect.any(Number),
+          endDate: expect.any(String),
+          bids: [],
+          user: user1._id.toString(),
+          __v: 0
+        },
+        {
+          _id: expect.anything(),
+          title: expect.any(String),
+          description: expect.any(String),
+          quantity: expect.any(Number),
+          endDate: expect.any(String),
+          bids: [],
+          user: user1._id.toString(),
+          __v: 0
+        }]);
+      });
+  });
+
+  
   it('creates an auction', async() => {
     const user1 = await User.create({
       email: 'jj@gmail.com',
